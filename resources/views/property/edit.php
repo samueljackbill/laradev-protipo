09f171851d3a5c5ed350de93ff61a57dcf25198b
Samuel Pires <samueljackbill@gmail.com>
2025-04-22 17:36:15 -0300

    Add files via upload

--- resources/views/property/edit.php
+++ resources/views/property/edit.php
@@ -1,4 +1,24 @@
-<h1>Editando Imóvel</h1>
+<h1>Formulário de Edição :: Imóveis</h1>
 
 <p><a href="<?= url('/imoveis'); ?>">Listar imóveis</a></p>
-<p><a href="<?= url('/imoveis/editar_imovel/' . $name); ?>">Editar imóvel</a></p>
+
+<?php  $property = $property[0]; ?>
+
+<form action="<?= url('/imoveis/update'); ?>" method="put">
+
+    <?= csrf_field(); ?>
+
+    <label for="title">Título do Imóvel:</label><br>
+    <input type="text" id="title" name="title" value="<?= $property->title; ?>" required><br><br>
+
+    <label for="description">Descrição:</label><br>
+    <textarea id="description" name="description" rows="10" cols="50"><?= $property->description; ?></textarea><br><br>
+
+    <label for="rental_price">Valor de Locação:</label><br>
+    <input type="number" id="rental_price" name="rental_price" step="0.01" value="<?= $property->rental_price; ?>"><br><br>
+
+    <label for="sale_price">Valor de Compra:</label><br>
+    <input type="number" id="sale_price" name="sale_price" step="0.01" value="<?= $property->sale_price; ?>"><br><br>
+
+    <button type="submit">Cadastrar Imóvel</button>
+</form>
